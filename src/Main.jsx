29b86a6345b7f1e90744e82edc26d6447f0f73d4
Emil Barranco <60144827+emilbarranco@users.noreply.github.com>
2020-04-27 13:33:52 -0400

    Sidebar Created
    - Sidebar created.
    - Main component stylesheet completed.
    - Link routes will be added on next updates.

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,6 +4,22 @@ import './styles/main_style.css';
 function Main() {
   return (
     <div className="Main">
+      <input type="checkbox" id="Check"/>
+      <label for="Check">
+        <ion-icon name="menu" id="Btn"></ion-icon>
+        <ion-icon name="close" id="Cancel"></ion-icon>
+      </label>
+      <div className="Sidebar">
+        <header>Movie Manager</header>
+        <ul>
+          <li><a href="#"><ion-icon name="home"></ion-icon>Actors</a></li>
+          <li><a href="#"><ion-icon name="videocam"></ion-icon>Movies</a></li>
+          <li><a href="#"><ion-icon name="information-circle"></ion-icon>More</a></li>
+        </ul>
+      </div>
+      <section>
+        <h1>Dashboard</h1>
+      </section>
     </div>
   );
 }
