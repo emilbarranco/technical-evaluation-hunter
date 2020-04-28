d08cff209550947f142b9147b9e275c28e4867e3
Emil Barranco <60144827+emilbarranco@users.noreply.github.com>
2020-04-27 21:17:30 -0400

    CRUD Operations Completed.
    - CRUD Operations for Server Requests have been completed.
    - Form's connection to the server will be developed on next update.

--- src/server/index.js
+++ src/server/index.js
@@ -21,13 +21,13 @@ App.listen(5000, () => {
 App.post('/actors', async (req, res) => {
     try {
 
-        // Destructuring the content from the body to send through the query
+        // Destructuring the content from the body to it send through the query
         const { Fullname, Gender, Birthdate } = req.body
         const newActor = await Pool.query(
             'INSERT INTO ACTORS (Fullname, Gender, Birthdate) VALUES ($1, $2, $3) RETURNING *', 
             [Fullname, Gender, Birthdate]
         )
-
+        // Retrieving the response from the server
         res.json(newActor.rows[0])
 
     } catch (error) {
@@ -46,7 +46,44 @@ App.get('/actors', async (req, res) => {
 });
 
 // Getting a Single Actor
+App.get('/actors/:ActorID', async (req, res) => {
+    try {
+        const { ActorID } = req.params
+        const actor = await Pool.query(
+            'SELECT * FROM ACTORS WHERE ActorID = $1', 
+            [ActorID]
+        )
+        res.json(actor.rows[0])
+    } catch (error) {
+        console.error(error.message)
+    }
+});
 
+// Updating an Actor
+App.put('/actors/:ActorID', async (req, res) => {
+    try {
+        const { ActorID } = req.params
+        const { Fullname, Gender, Birthdate } = req.body
+        const updateActor = await Pool.query(
+            "UPDATE ACTORS SET Fullname = $1, Gender = $2, Birthdate = $3 WHERE ActorID = $4", 
+            [Fullname, Gender, Birthdate, ActorID]
+        )
+        res.json("Actor Updated.")
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 // Deleting Actors from the table
-
+App.delete('/actors/:ActorID', async (req, res) => {
+    try {
+        const { ActorID } = req.params
+        const deleteActor = await Pool.query(
+            "DELETE FROM ACTORS WHERE ActorID = $1", 
+            [ActorID]
+        )
+        res.json("Actor Deleted.")
+    } catch (error) {
+        console.error(error.message)
+    }
+})
